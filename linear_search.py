a92be8d2d9fc2b3aa394e2683fc5294bd42feb6b
ryan-newby <32687734+ryan-newby@users.noreply.github.com>
2022-09-27 18:04:37 -0600

    Create linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,14 @@
+myArray = [2, 3, 6, 9, 12, 15, 19]
+
+def search(elm,low,high, myArray):
+   index = low
+   while(index <= high):
+      if myArray[index] == elm:
+         return "elm " + str(elm) + " found at index " + str(index) + "!!!"
+      else: 
+        print("not found at index " + str(index))
+        index = index + 1
+    
+   return "elm not found"
+
+print(search(12, 0, 6, myArray))
